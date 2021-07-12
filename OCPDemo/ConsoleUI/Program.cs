a51d0817935903b0bd67adcb6bccd130aa58119a
Pankaj Rayal <https://github.dxc.com/>
2021-07-12 11:58:04 +0530

    O -OCP - Open/Closed Principle setup

---
+++ OCPDemo/ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Udit", LastName = "Bhandari" },
+                new PersonModel { FirstName = "Rajeev", LastName = "Singh" },
+                new PersonModel { FirstName = "Himanshu", LastName = "Sharma" },
+                new PersonModel { FirstName = "Pramod", LastName = "Singh" },
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach(var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+
+            foreach(var emp in employees)
+            {
+                Console.WriteLine($"{emp.FirstName} {emp.LastName}: {emp.EmailAddress}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
